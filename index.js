763128cbb3b95584e2585f0da2cc8abee7b2e648
yottaaaaaa <117156331+yottaaaaaa@users.noreply.github.com>
2024-04-08 16:14:43 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-const tutorials = [
-  'what does the this keyword mean?',
-  'What is the Constructor OO pattern?',
-  'implementing Blockchain Web API',
-  'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
-  'What is the difference between stopPropagation and preventDefault?',
-  'Immutable State and Pure Functions',
-  'what is the difference between == and ===?',
-  'what is the difference between event capturing and bubbling?',
-  'what is JSONP?'
-];
+
+  let tutorials = [
+      "what does the this keyword mean?",--"The term keyword refers to the object it belongs to in JavaScript.",
+      "What is the Constructor OO pattern?",--"The Constructor OO pattern involves using constructors to create objects in JavaScript.",
+      "implementing Blockchain Web API",--"Blockchain Web API implementation involves integrating blockchain functionality into web applications.",
+      "The Test Driven Development Workflow",--"Test Driven Development is a software development approach where tests are written before the actual code.",
+      "What is NaN and how Can we Check for it",--"NaN stands for 'Not a Number' and can be checked using the isNaN() function in JavaScript.",
+      "What is the difference between stopPropagation and preventDefault?",--"stopPropagation() prevents further propagation of the current event in the capturing and bubbling phases.",
+      "Immutable State and Pure Functions",--"Immutable state and pure functions are concepts in functional programming.",
+      "what is the difference between == and ===?",--"== is a loose equality comparison operator, while === is a strict equality comparison operator in JavaScript.",
+      "what is the difference between event capturing and bubbling?",--"Event capturing and bubbling are two phases of event propagation in the DOM.",
+      "what is JSONP?",--"JSONP (JSON with Padding) is a technique used to request data from a server in a different domain in JavaScript.",
+ ];
+
 
 const titleCased = () => {
   return tutorials
